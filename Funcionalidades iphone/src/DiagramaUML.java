33ed4d5a9566d30efb9927e41e3568a31bdc2ebe
Eduardo Fonseca <edufonseca90@gmail.com>
2024-05-27 22:42:32 -0300

    DiagramaUML

---
+++ Funcionalidades iphone/src/DiagramaUML.java
@@ -0,0 +1,26 @@
+classDiagram
+    class ReprodutorMusical {
+        +tocar()
+        +pausar2()
+        +sellecionarMusica(String Musica)
+    }
+
+    class AparelhoTelefonico {
+        +ligar(String numero)
+        +atender()
+        +iniciarCorreioVoz()
+    }
+
+    class NavegadorInternet {
+        +exibirPagina(String url)
+        +adicionarNovaAba()
+        +atualizarPagina()
+    }
+
+    class iPhone {
+    }
+
+    iPhone --> ReprodutorMusical
+    iPhone --> AparelhoTelefonico
+    iPhone --> NavegadorInternet
+
